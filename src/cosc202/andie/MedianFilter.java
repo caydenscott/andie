0268f32fac0436cb73cc972a1b086ee2f82ff2de
Cayden Scott <scoca589@student.otago.ac.nz>
2023-03-29 20:20:25 +1300

    changed a variable

--- src/cosc202/andie/MedianFilter.java
+++ src/cosc202/andie/MedianFilter.java
@@ -5,23 +5,8 @@ import java.awt.image.*;
 import java.util.*;
 
 /**
- * <p>
- * ImageOperation to apply a Mean (simple blur) filter.
- * </p>
- * 
- * <p>
- * A Mean filter blurs an image by replacing each pixel by the average of the
- * pixels in a surrounding neighbourhood, and can be implemented by a
- * convoloution.
- * </p>
- * 
- * <p>
- * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA
- * 4.0</a>
- * </p>
- * 
  * @see java.awt.image.ConvolveOp
- * @author Steven Mills
+ * @author Cayden Scott
  * @version 1.0
  */
 public class MedianFilter implements ImageOperation, java.io.Serializable {
@@ -34,16 +19,10 @@ public class MedianFilter implements ImageOperation, java.io.Serializable {
 
     /**
      * <p>
-     * Construct a Mean filter with the given size.
+     * Construct a Median filter with the given size.
      * </p>
      * 
-     * <p>
-     * The size of the filter is the 'radius' of the convolution kernel used.
-     * A size of 1 is a 3x3 filter, 2 is 5x5, and so on.
-     * Larger filters give a stronger blurring effect.
-     * </p>
-     * 
-     * @param radius The radius of the newly constructed MeanFilter
+     * @param radius The radius of the newly constructed MedianFilter
      */
     MedianFilter(int radius) {
         this.radius = radius;
@@ -51,14 +30,14 @@ public class MedianFilter implements ImageOperation, java.io.Serializable {
 
     /**
      * <p>
-     * Construct a Mean filter with the default size.
+     * Construct a median filter with the default size.
      * </p
      * >
      * <p>
-     * By default, a Mean filter has radius 1.
+     * By default, a median filter has radius 1.
      * </p>
      * 
-     * @see MeanFilter(int)
+     * @see medianfilter(int)
      */
     MedianFilter() {
         this(1);
@@ -76,20 +55,21 @@ public class MedianFilter implements ImageOperation, java.io.Serializable {
         
         /**
          * had issues with black chunk being cut off at bottom of image. to fix, i added padding pixels around the outside
-         * of the image, such that they would get cut off instead. then just crop the image back to its original size.
+         * of the image, such that they would get cut off instead. then just crop the image back to its original size. 
+         * a padding amout of 1 has shown to be sufficient.
          */
 
         // Choose how many extra pixels to add around the edges
-        int padAmount = radius/2;
+        int padAmount = 1;
         
         // Create a new image with padding around the edges
-        BufferedImage paddedInput = new BufferedImage(width + padAmount * 2, height + padAmount * 2, input.getType());
-        Graphics2D g = paddedInput.createGraphics();
+        BufferedImage padding = new BufferedImage(width + padAmount * 2, height + padAmount * 2, input.getType());
+        Graphics2D g = padding.createGraphics();
         g.drawImage(input, padAmount, padAmount, null);
         g.dispose();
         
         // Get the pixels of the padded image
-        int[] pixels = paddedInput.getRGB(0, 0, width + padAmount * 2, height + padAmount * 2, null, 0, width + padAmount * 2);
+        int[] pixels = padding.getRGB(0, 0, width + padAmount * 2, height + padAmount * 2, null, 0, width + padAmount * 2);
         int[] neighbourhood;
         
         // Create a new output image with padding around the edges

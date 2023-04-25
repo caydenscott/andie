910bff72d8c29c7763f0a192b5d34575708ee483
Cayden Scott <scoca589@student.otago.ac.nz>
2023-04-26 10:14:02 +1200

    extended filters to handle edges with padding

--- src/cosc202/andie/GaussianFilter.java
+++ src/cosc202/andie/GaussianFilter.java
@@ -1,5 +1,6 @@
 package cosc202.andie;
 
+import java.awt.Graphics2D;
 import java.awt.image.*;
 
 public class GaussianFilter implements ImageOperation, java.io.Serializable {
@@ -51,11 +52,35 @@ public class GaussianFilter implements ImageOperation, java.io.Serializable {
      * @return The resulting gaussian blured image.
      */
     public BufferedImage apply(BufferedImage input) {
+        int height = input.getHeight();
+        int width = input.getWidth();
+    
+        int padAmount = radius; // Increase padding size for mirror padding
+    
+        // Create a new image with mirror padding around the edges
+        BufferedImage paddedInput = new BufferedImage(width + padAmount * 2, height + padAmount * 2, input.getType());
+        Graphics2D g = paddedInput.createGraphics();
+        g.drawImage(input, padAmount, padAmount, null);
+        // Mirror left and right edges
+        g.drawImage(input, 0, padAmount, padAmount, height + padAmount, 0, 0, padAmount, height, null);
+        g.drawImage(input, width + padAmount, padAmount, width + padAmount * 2, height + padAmount, width - padAmount, 0, width, height, null);
+        // Mirror top and bottom edges
+        g.drawImage(input, padAmount, 0, width + padAmount, padAmount, 0, 0, width, padAmount, null);
+        g.drawImage(input, padAmount, height + padAmount, width + padAmount, height + padAmount * 2, 0, height - padAmount, width, height, null);
+        // Mirror corners
+        g.drawImage(input, 0, 0, padAmount, padAmount, 0, 0, padAmount, padAmount, null);
+        g.drawImage(input, width + padAmount, 0, width + padAmount * 2, padAmount, width - padAmount, 0, width, padAmount, null);
+        g.drawImage(input, 0, height + padAmount, padAmount, height + padAmount * 2, 0, height - padAmount, padAmount, height, null);
+        g.drawImage(input, width + padAmount, height + padAmount, width + padAmount * 2, height + padAmount * 2, width - padAmount, height - padAmount, width, height, null);
+        g.dispose();
+    
+        BufferedImage output = new BufferedImage(width, height, input.getType());
+    
         int size = (2 * radius + 1); // calculates size of filter
         float[] array = new float[size * size]; // array to store filter values
         float sigma = radius / 3.0f; // calculate sigma value
         float sum = 0.0f; // variable to keep track of sum of filter values
-        //populate array with calculated values
+        // populate array with calculated values
         for (int y = -radius; y <= radius; y++) {
             for (int x = -radius; x <= radius; x++) {
                 int index = (y + radius) * size + (x + radius);
@@ -64,17 +89,22 @@ public class GaussianFilter implements ImageOperation, java.io.Serializable {
                 sum += array[index];
             }
         }
-        //normalise values by dividing by sum
+        // normalise values by dividing by sum
         for (int i = 0; i < array.length; i++) {
             array[i] /= sum;
         }
         Kernel kernel = new Kernel(2 * radius + 1, 2 * radius + 1, array);
         ConvolveOp convOp = new ConvolveOp(kernel);
-        BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null),
-                input.isAlphaPremultiplied(), null);
-        convOp.filter(input, output);
-
+        BufferedImage filteredImage = convOp.filter(paddedInput, null);
+        
+        // Create a new image to store the filtered pixels without padding
+        output = new BufferedImage(width, height, input.getType());
+        g = output.createGraphics();
+        g.drawImage(filteredImage, -padAmount, -padAmount, null);
+        g.dispose();
+        
         return output;
     }
+    
 
 }
